9ba9825000a5afd00a198ec6451bd9df85c165e4
Jeremmy <hizkiajeremmy@gmail.com>
2024-06-10 16:36:19 +0700

    setups

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        SfProDisplay: ["SfProDisplay"]
+      },
+      colors: {
+        'cust-black' : '#121212',
+        'cust-white' : '#FAFAFA',
+
+        'cust-pink-lightest' : '#FFF5FB',
+        'cust-pink-lighter' : '#FAA0DA',
+        'cust-pink-light' : '#ED5CBA',
+        'cust-pink-normal' : '#D7319D',
+        'cust-pink-dark' : '#B3197D',
+        'cust-pink-darker' : '#870E5D',
+
+        'cust-blue-lightest' : '#F5F5FF',
+        'cust-blue-lighter' : '#A2A0FA',
+        'cust-blue-light' : '#5E5CED',
+        'cust-blue-normal' : '#3431D7',
+        'cust-blue-dark' : '#1B19B3',
+        'cust-blue-darker' : '#100E87',
+      }
+    },
   },
   plugins: [],
 }
